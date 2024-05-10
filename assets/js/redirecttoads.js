57ffa81369f0b7f8b2dff2451303141afbb86717
caessarrr <saintcaessar@gmail.com>
2024-05-11 02:10:12 +0700

    add

--- assets/js/redirecttoads.js
+++ assets/js/redirecttoads.js
@@ -1,19 +1,11 @@
 // assets/js/redirecttoads.js
 
-// Cek apakah sudah ada data dalam local storage
-if (!localStorage.getItem('redirected')) {
-    // Jika belum ada, set data redirect ke true
-    localStorage.setItem('redirected', 'true');
-
-    // set 1 detik sebelum alert
-    setTimeout(function () {
-    // Tampilkan alert
-    alert('Lewati link iklan sebelum lanjut, klik OK ...');
-    // Redirect ke halaman redirect
+// Cek apakah pengguna pernah mengunjungi halaman iklan sebelumnya
+if (!sessionStorage.getItem('visitedAdPage')) {
+    // Jika tidak, arahkan ke halaman iklan
+    sessionStorage.setItem('visitedAdPage', 'true'); // Set data di session storage untuk menandai kunjungan pertama ke halaman iklan
     window.location.href = 'https://caessarrr.github.io/webstore/assets/pages/ads.html';
-    },1000);
-
 } else {
-    // Jika sudah ada data dalam local storage, hapus data tersebut
-    // localStorage.removeItem('redirected');
+    // Jika iya, arahkan kembali ke halaman utama
+    window.location.href = 'https://caessarrr.github.io/webstore/index.html';
 }
--- assets/js/redirecttoindex.js
+++ assets/js/redirecttoindex.js
@@ -1,9 +1,10 @@
 // assets/js/redirecttoindex.js
 
-// Hapus data redirect dari local storage
-localStorage.removeItem('redirected');
-// Tunggu 3 detik sebelum redirect kembali ke halaman index
+// Tunggu 2 detik sebelum redirect kembali ke halaman index
 setTimeout(function () {
-    // Redirect kembali ke halaman index
-    window.location.href = 'https://caessarrr.github.io/webstore/index.html';
-}, 100);
+    // Cek apakah pengguna pernah mengunjungi halaman iklan
+    if (!sessionStorage.getItem('visitedAdPage')) {
+        // Jika tidak, arahkan kembali ke halaman index
+        window.location.href = 'https://caessarrr.github.io/webstore/index.html';
+    }
+}, 2000);
